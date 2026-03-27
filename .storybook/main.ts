70ba64a9934ce25afdeea8a71f4b0b6149e275f9
Sumit Sharma <sumitss92@yahoo.com>
2026-03-27 13:44:27 +0530

    storybook/chromatic error fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
   "addons": [
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
+    "@storybook/addon-vitest",
   ],
   "framework": "@storybook/react-vite"
 };
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,4 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from '@storybook/react'
 import '../src/styles/global.css'
 
 const preview: Preview = {
